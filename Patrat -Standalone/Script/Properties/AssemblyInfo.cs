6a1bdaf76ca43a97c2725e861c50766b929f933f
patrat46 <patrat46@gmail.com>
2021-12-09 17:48:42 -0800

    Add files via upload

---
+++ Patrat -Standalone/Script/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Anti AFK Kick")]
+[assembly: AssemblyDescription("Prevents FFXIV idle timer from Kicking you.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Anti AFK Kick")]
+[assembly: AssemblyCopyright("Copyright © https://github.com/Eternita-S 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b4c67313-975d-4f3d-aefd-003272caefc5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// Project by Eternita-S @ Grithub https://github.com/Eternita-S/AntiAfkKick 
+[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
